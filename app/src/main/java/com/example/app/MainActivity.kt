51455d8ee4dc73a903fbf695008e4728629fbaec
jankowskyy <konrad.maksymilian.jankowski@gmail.com>
2023-09-15 08:38:05 +0200

    .

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
+        findViewById<Button>(R.id.MojPrzycisk).setOnClickListener{
+            findViewById<Button>(R.id.MojPrzycisk).text = "Dziala"
+        }
     }
 }
